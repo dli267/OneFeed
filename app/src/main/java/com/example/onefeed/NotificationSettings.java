501414eae79eeb490886d553467ec378bb8f9139
Sommer Olson <37749455+Olsosom@users.noreply.github.com>
2020-05-03 12:12:18 -0500

    Fix issue with file

--- app/src/main/java/com/example/onefeed/NotificationSettings.java
+++ app/src/main/java/com/example/onefeed/NotificationSettings.java
@@ -1,8 +1,11 @@
+package com.example.onefeed;
+
 public class NotificationSettings {
-    /*
+
+    //should have been here originally, not in test
+     /*
     // Would have been an activity to select and change settings for specific types of
     // notifications and customize further how they show up for the user.
     // Had issues adding as an activity, because I kept getting and IDE error
     */
-
 }
