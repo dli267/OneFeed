753ea1248f811eb8b3fa0917599ceccacdbdb2db
dli267@wisc.edu <Quartermoon2>
2020-05-03 07:51:34 -0500

    Added settings

--- app/src/main/java/com/example/onefeed/MainActivity.java
+++ app/src/main/java/com/example/onefeed/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.onefeed;
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
@@ -51,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // ask for internet permission
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
         }
 
@@ -180,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -190,11 +193,20 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handles going to the appropriate page from the menu
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent2 = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent2);
+                break;
+            case R.id.profile:
+                Intent intent1 = new Intent(this, AccountActivity.class);
+                this.startActivity(intent1);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/onefeed/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.example.onefeed;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new MySettingsFragment())
+                .commit();
+    }
+}
